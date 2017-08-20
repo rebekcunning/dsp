1e1ea5d3ed8d6671792292383c5bb90ece221fa9
rebekcunning <bekcunning@gmail.com>
2017-08-20 15:31:43 -0700

    completed python lists assignment

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -15,8 +15,12 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
-
+    count = 0
+    for word in words:
+        if len(word) >= 2:
+            if word[0] == word[-1]:
+                count += 1
+    return count
 
 def front_x(words):
     """
@@ -32,7 +36,14 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    x_words = []
+    other_words = []
+    for word in words:
+        if word[0] == "x":
+            x_words.append(word)
+        else:
+            other_words.append(word)
+    return sorted(x_words) + sorted(other_words)
 
 
 def sort_last(tuples):
@@ -49,8 +60,8 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
 
+    return sorted(tuples, key=lambda x: x[1])
 
 def remove_adjacent(nums):
     """
@@ -68,8 +79,13 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
-
+    i = 0
+    new_list = []
+    for num in nums:
+        if i != num:
+            new_list.append(num)
+        i = num
+    return new_list
 
 def linear_merge(list1, list2):
     """
@@ -85,4 +101,5 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    new_list = list1 + list2
+    return sorted(new_list)
