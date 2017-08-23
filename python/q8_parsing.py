08feb52e5dcf3993685e0f1064f086b4063198c0
rebekcunning <bekcunning@gmail.com>
2017-08-23 15:36:10 -0700

    completed q8 exercise

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -3,4 +3,7 @@
 # goals scored for and against each team in that season (so Arsenal scored 79 goals 
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
-
+import pandas as pd
+df = pd.read_csv('football.csv')
+df['diff'] = abs(df['Goals']-df['Goals Allowed'])
+print(df['Team'][df['diff'].argmin()])
